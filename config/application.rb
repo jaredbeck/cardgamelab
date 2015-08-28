0a998c936fc08f1b712593a7eda0b8aab67d9303
Jared Beck <jared@jaredbeck.com>
2015-08-28 15:06:56 -0400

    Follow dotenv multi-env convention
    https://github.com/bkeepers/dotenv#multiple-rails-environments

--- config/application.rb
+++ config/application.rb
@@ -10,15 +10,6 @@ require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 
-# In local development, load from e.g. environments/test.env and .env
-# http://devblog.avdi.org/2014/01/17/dotenv-for-multiple-environments/
-if Rails.env.development? || Rails.env.test?
-  Dotenv.overload(
-    File.expand_path("../environments/#{Rails.env}.env", __FILE__),
-    File.expand_path("../../.env", __FILE__)
-  )
-end
-
 module CGL
   class Application < Rails::Application
     config.time_zone = 'UTC'
